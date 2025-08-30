d65ad555c99ea4cdd7d24cfae8f856394a018be1
mangovina <92ant.k@gmail.com>
2025-08-30 15:28:33 +0700

    08/30

--- src/components/StructuredData.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/StructuredData.tsx
-export default function StructuredData({ json }: { json: unknown }) {
-    return (
-        <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(json) }}
-        />
-    );
-}
